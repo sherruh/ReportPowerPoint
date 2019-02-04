6a41546d45dde78db1d6e0261c664c333df07b10
sherruh <sherruh@gmail.com>
2019-02-04 23:56:28 +0600

    calc rectangle from note home commit

---
+++ src/main/java/Work/App.java
@@ -0,0 +1,14 @@
+package Work;
+
+import java.io.File;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        CreateFilePowerPoint filePpt=new CreateFilePowerPoint("Test1");
+        CreateSlide createSlide=new CreateSlide(filePpt.getFile());
+        createSlide.addEmptySlide();
+        createSlide.add2ScreenshotsSlide();
+    }
+}
---
+++ src/main/java/Work/CreateSlide.java
@@ -0,0 +1,74 @@
+package Work;
+
+import org.apache.poi.util.IOUtils;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFPictureData;
+import org.apache.poi.xslf.usermodel.XSLFPictureShape;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.io.*;
+
+public class CreateSlide {
+    private File file;
+    private FileInputStream inputStream;
+    private XMLSlideShow ppt;
+    private CalcRectangleFor2Screenshots calcRectangleFor2Screenshots;
+    public CreateSlide(File file){
+        this.file=file;
+        try {
+            inputStream = new FileInputStream(this.file);
+            ppt = new XMLSlideShow(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        calcRectangleFor2Screenshots=new CalcRectangleFor2Screenshots();
+    }
+
+    public void addEmptySlide(){
+        ppt.createSlide();
+        System.out.println("Added empty slides successfully!");
+    }
+
+    public void add2ScreenshotsSlide() {
+        try {
+            XSLFSlide slide = ppt.createSlide();
+            for (int i = 1; i < 3; i++) {
+                File image = new File(i + ".jpg");
+                byte[] picture = new byte[0];
+                picture = IOUtils.toByteArray(new FileInputStream(image));
+                int idx = ppt.addPicture(picture, XSLFPictureData.PICTURE_TYPE_JPEG);
+                XSLFPictureShape pic = slide.createPicture(idx);
+                //pic.setAnchor(new Rectangle((365*(i-1)),120,355,350));
+                calcRectangleFor2Screenshots.getRightRectangle(pic.getAnchor());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        writeToFile("Added slide with 2 screenshots successfully!");
+    }
+
+    public void writeToFile(String message){
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+            ppt.write(out);
+            System.out.println(message);
+            out.close();
+            openFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void openFile(){
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().open(file);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
