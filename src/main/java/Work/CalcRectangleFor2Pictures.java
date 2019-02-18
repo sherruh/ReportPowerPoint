d8116b7b337f9cc7939ec7cc1de7eb28012d27f9
sherruh <sherruh@gmail.com>
2019-02-18 22:26:52 +0600

    add logic to height more

--- src/main/java/Work/CalcRectangleFor2Pictures.java
+++ src/main/java/Work/CalcRectangleFor2Pictures.java
@@ -8,7 +8,7 @@ public class CalcRectangleFor2Pictures {
     public Rectangle getRightRectangle(Rectangle2D pictureRectangle,int numberOfPicture){
         double curWidth;
         double curHeight;
-        final double rightWidth=700;
+        final double rightWidth=1000;
         final double rightHeight=1000;
         final double centerOfSlide=1024;
         double deltaX=numberOfPicture-1;
@@ -16,15 +16,22 @@ public class CalcRectangleFor2Pictures {
         curHeight=pictureRectangle.getHeight();
         double rate=curWidth/curHeight;
         while(!checkSize(curWidth,curHeight,rightWidth,rightHeight)){
-            curWidth--;
-            curHeight=curHeight-1/rate;
+            //TODO add logic to height>width
+            if (curWidth<rightWidth){
+                curWidth++;
+                curHeight=curHeight+1/rate;
+            }else if(curWidth>rightWidth) {
+                curWidth--;
+                curHeight=curHeight-1/rate;
+            }
+
         }
         double xPosition=centerOfSlide*deltaX+Math.abs(centerOfSlide-curWidth)/2;
         return new Rectangle((int)xPosition,400,(int)curWidth,(int)curHeight);
     }
 
     protected boolean checkSize(double curWidth, double curHeight,double rightWidth,double rightHeight) {
-        if (curWidth<=rightWidth && curHeight<=rightHeight){
+        if (curWidth==rightWidth){
             return true;
         }
         return false;
