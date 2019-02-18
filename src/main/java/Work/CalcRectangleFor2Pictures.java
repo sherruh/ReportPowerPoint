44a6035485fbab8b4a5b73a27509565c41970607
sherruh <sherruh@gmail.com>
2019-02-18 22:20:14 +0600

    calc rectangle calc size finished from note home commit

--- src/main/java/Work/CalcRectangleFor2Pictures.java
+++ src/main/java/Work/CalcRectangleFor2Pictures.java
@@ -8,19 +8,19 @@ public class CalcRectangleFor2Pictures {
     public Rectangle getRightRectangle(Rectangle2D pictureRectangle,int numberOfPicture){
         double curWidth;
         double curHeight;
-        final double rightWidth=355;
-        final double rightHeight=350;
-        final double centerOfSlide=360;
+        final double rightWidth=700;
+        final double rightHeight=1000;
+        final double centerOfSlide=1024;
         double deltaX=numberOfPicture-1;
         curWidth=pictureRectangle.getWidth();
         curHeight=pictureRectangle.getHeight();
         double rate=curWidth/curHeight;
         while(!checkSize(curWidth,curHeight,rightWidth,rightHeight)){
-            curWidth-=1;
+            curWidth--;
             curHeight=curHeight-1/rate;
         }
         double xPosition=centerOfSlide*deltaX+Math.abs(centerOfSlide-curWidth)/2;
-        return new Rectangle((int)xPosition,120,(int)curWidth,(int)curHeight);
+        return new Rectangle((int)xPosition,400,(int)curWidth,(int)curHeight);
     }
 
     protected boolean checkSize(double curWidth, double curHeight,double rightWidth,double rightHeight) {
